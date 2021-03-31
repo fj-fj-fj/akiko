91aba8d920eb1b0185cb82d02a20bad053bc4c98
fj-fj-fj <berendeyzhiv@gmail.com>
2021-03-31 21:06:10 +0300

    bot: correct style, add logging, typing

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
-
-"""This module contains class Bot"""
 import json
-import sys
-import time
+from logging import Logger
+from typing import Any, Dict
 
 from api.coinmarketcap.core import CoinDataFetcher
 from db import shelve_db
 from utils.webhook import configurate_webhook
 
+logger: Logger = Logger(__file__)
+
 
 class Bot(CoinDataFetcher):
-    _CALLBACK_QUERY_ID = 0
+
+    _CALLBACK_QUERY_ID: int = 0
 
     def __init__(self, **kwargs: dict) -> None:
         super().__init__(**kwargs['coinmarketcap'])
@@ -22,45 +22,43 @@ class Bot(CoinDataFetcher):
     def update_webhook(self) -> None:
         configurate_webhook(self)
 
-    def send_message(self, chat_id: int, text: str) -> str:
-        """The funcion sends users a message.
+    def send_message(self, chat_id: int, text: str) -> Dict[str, Any]:
+        """
+        This method sends to user a message.
 
-        :param chat_id: (int)chat ID
-        :param: (str)text: coin price and/or coin information, or error message 
-        :return: (dict)requests.models.Response
+        Args:
+            chat_id: chat ID
+
+        Returns:
+            message: Coin price (and/or information), or some error
 
         """
-        print(f'\nBot.send_message(chat_id={chat_id}, text={text})\n', file=sys.stderr)
+        logger.debug(f'{chat_id=}, {text=}')
 
         url = Bot.URL + 'sendMessage'
-        data = {
-            'chat_id': chat_id, 
-            'text': ''
-        }
+        data = {'chat_id': chat_id, 'text': ''}
         
         if Bot._CALLBACK_QUERY_ID or 'error' in text:
             data['text'] += text
-            r = self.session.post(url, json=data)
             Bot._CALLBACK_QUERY_ID = 0
-            return r.json()
-        
+            return self.session.post(url, json=data).json()
+
         if 'Akiko' in text:
             data['parse_mode'] = 'markdown'
             data['text'] += f"{text}{'(‾◡◝)':>40}"
-            r = self.session.post(url, json=data)
-            return r.json()
+            return self.session.post(url, json=data).json()
 
         if '$' in text:
+            bitcoin_fetched = shelve_db.fetch_last_coin_id(chat_id) == 1
             data['parse_mode'] = 'markdown'
             data['text'] += text
             data['reply_markup'] = {
-                    'inline_keyboard': [
-                        [{'text': '฿uy' if shelve_db.fetch_last_coin_id(chat_id) == 1 else 'Buy',
-                          'callback_data': 'In the near future you can do it'}]
-                    ]
+                'inline_keyboard': [[
+                    {'text': '฿uy' if bitcoin_fetched else 'Buy',
+                    'callback_data': 'In the near future you can do it'}
+                ]]
             }
-            r = self.session.post(url, json=data)
-            return r.json()
+            return self.session.post(url, json=data).json()
 
         text = json.loads(text)
         data['disable_web_page_preview'] = True
@@ -68,17 +66,15 @@ class Bot(CoinDataFetcher):
         id_coin, name_coin = text.pop('🗝')
 
         if name_coin == "GoHelpFund":
-            h = 'HELP it\'s GoHelpFund ticker\nEnter /help or h if you need help\n\n'
-            data['text'] += h
+            data['text'] += 'HELP it\'s GoHelpFund ticker\n \
+                             Enter /help or h if you need help\n\n'
 
         for key, value in text.items():
             if not value: continue
             value = ' | '.join([i for i in value])
-            data['text'] += key + ' ▫️ ' + value + '\n\n'
+            data['text'] += key + ' - ' + value + '\n\n'
 
         kiss = 'H( ^ _ - )DL' if id_coin == 1 else "it's altc(´◡`)in"
         data['text'] += kiss
 
-        r = self.session.post(url, json=data)
-        
-        return r.json()
+        return self.session.post(url, json=data).json()
