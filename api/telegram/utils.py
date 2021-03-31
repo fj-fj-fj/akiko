22cdcd1736127c992256b63e040bd6d0312f1e02
fj-fj-fj <berendeyzhiv@gmail.com>
2021-03-31 22:21:11 +0300

    bot: form a response to the user separately from `main`

---
+++ api/telegram/utils.py
@@ -0,0 +1,51 @@
+import re
+from logging import Logger
+from typing import Optional, Tuple
+
+from db import shelve_db
+
+logger: Logger = Logger(__file__)
+
+
+def form_response_to_user(bot: object, r: dict) -> Tuple[str, str]:
+    try:
+        chat_id: str = r['message']['chat']['id']
+        message: str = r['message']['text'].lower().strip()
+
+        user_has_session: bool = shelve_db.has_user_session(chat_id)
+
+        user_wants_more_info: bool = message in \
+            ('information', '/info', 'info', 'i')
+        user_needs_help: bool = message in ('/start', '/help', 'h', 'sos')
+
+        logger.debug(f'{message=}, {user_wants_more_info=}, {user_has_session=}')  # noqa: E501
+
+        if user_wants_more_info and not user_has_session:
+            data: str = shelve_db.display_error_message('session')
+        elif user_wants_more_info and user_has_session:
+            id_coin: int = shelve_db.fetch_last_coin_id(chat_id)
+            data: str = bot.display_coin_info(id_coin)
+        elif user_needs_help:
+            # re because multi-line messages preserve spaces
+            data = re.sub(r'((?!\n)\s+)', ' ', """I'm *Akiko* and i use coinmarketcap API\n
+                    To get the coin price, send me its name or ticker.
+                    (e.g. `Dogecoin` | `Doge`, `Bitcoin` | `BTC`)\n
+                    For more information about coin, enter `info` or `i`""")
+        else:
+            id_coin: Optional[int] = bot.fetch_coin_id(message)
+            data = bot.extract_coin_price(id_coin) if id_coin \
+                else shelve_db.display_error_message('match')
+            shelve_db.touch_session(chat_id, id_coin)  # create or update
+
+    except KeyError:
+        bot._CALLBACK_QUERY_ID: str = r['callback_query']['id']
+        message: str = r['callback_query']['message']
+        chat_id: str = message['chat']['id']
+        callback_data: str = message['reply_markup']['inline_keyboard'][0][0]['callback_data']  # noqa: E501
+
+        data: str = callback_data if callback_data else \
+            shelve_db.display_error_message('unknown')
+
+        logger.debug(f'{bot._CALLBACK_QUERY_ID=}, {message=}, {data=}')
+
+    return (chat_id, data)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import re
 from logging import Logger
 
 from flask import Flask
@@ -6,8 +5,8 @@ from flask import request
 from flask import jsonify
 
 from api.telegram.bot import Bot
+from api.telegram.utils import form_response_to_user
 from config import CONFIG_KWARGS
-from db import shelve_db
 
 logger: Logger = logger(__file__)
 
@@ -33,46 +32,9 @@ def main() -> str:
     logger.debug(' * BOT STARTED * '.center(130, '~'))
 
     if request.method == 'POST':
-
         r = request.get_json()
-
-        try:
-            chat_id = r['message']['chat']['id']
-            message = r['message']['text'].lower().strip()
-
-            session = shelve_db.has_user_session(chat_id)
-
-            need_more_info = message in ('information', '/info', 'info', 'i')
-            need_help = message in ('/start', '/help', 'h', 'sos')
-
-            logger.debug(f'{message=}, {need_more_info=}, {session=}')
-                
-            if need_more_info and not session:
-                data: str = shelve_db.display_error_message('session')
-            elif need_more_info and session:
-                id_coin: int = shelve_db.fetch_last_coin_id(chat_id)
-                data: str = bot.display_coin_info(id_coin)                
-            elif need_help:
-                # re because multi-line messages preserve spaces
-                data = re.sub(r'((?!\n)\s+)', ' ', f"""I'm *Akiko* and i use coinmarketcap API\n
-                       To get the coin price, send me its name or ticker.
-                       (e.g. `Dogecoin` | `Doge`, `Bitcoin` | `BTC`)\n
-                       For more information about coin, enter `info` or `i`""")
-            else:
-                id_coin = bot.fetch_coin_id(message) # int or None
-                data = bot.extract_coin_price(id_coin) if id_coin else shelve_db.display_error_message('match')
-                shelve_db.touch_session(chat_id, id_coin) # create or update
-
-        except KeyError:
-            bot._CALLBACK_QUERY_ID = r['callback_query']['id']
-            message = r['callback_query']['message']
-            chat_id = message['chat']['id']
-            callback_data = message['reply_markup']['inline_keyboard'][0][0]['callback_data']
-
-            data = callback_data if callback_data else shelve_db.display_error_message('unknown')
-
-            logger.debug(f'{bot._CALLBACK_QUERY_ID=}, {message=}, {data=}')
-
+        print(type(r))
+        chat_id, data = form_response_to_user(bot, r)
         bot.send_message(chat_id, text=data)
 
         logger.debug(f' * bot FINISHED * \n{jsonify(r)}\n\n')
